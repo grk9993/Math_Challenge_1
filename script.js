b393af0d619ba07a1730bcb699526b5c81ee9377
Rohan Karn <grk9993@outlook.com>
2024-03-06 11:12:57 +0530

    lcm & hcf added

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const buttons = document.querySelectorAll('button');
 buttons.forEach(button => {
     button.addEventListener('click', function() {
       const operation = this.id; 
-      const operations = ['addition', 'subtraction', 'multiplication', 'division'];
+      const operations = ['addition', 'subtraction', 'multiplication', 'division', 'lcm', 'hcf'];
       if (operations.includes(operation)) {
         window.location.href = operation + '.html'; 
       } else {
