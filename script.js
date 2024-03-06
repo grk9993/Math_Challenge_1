08a5def62095889980655814c3dd308fa3bb8862
Rohan Karn <grk9993@outlook.com>
2024-03-06 13:47:03 +0530

    roman +fraction

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ const buttons = document.querySelectorAll('button');
 buttons.forEach(button => {
     button.addEventListener('click', function() {
       const operation = this.id; 
-      const operations = ['addition', 'subtraction', 'multiplication', 'division', 'lcm', 'hcf'];
+      const operations = ['addition', 'subtraction', 'multiplication', 'division',
+         'lcm', 'hcf', 'roman', 'fraction_addition', 'fraction_subtraction'];
       if (operations.includes(operation)) {
         window.location.href = operation + '.html'; 
       } else {
